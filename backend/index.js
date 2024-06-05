9068cce4b2ef6b392795b3d3bf56119af98a49e2
Brilliant-Developer1 <email2obail@gmail.com>
2024-06-05 21:47:52 +0600

    Upadte 6

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,11 @@ app.use(cors(
     credentials: true,
   }
 ));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 app.use(express.json());
 
 function verifyToken(req, res, next) {
